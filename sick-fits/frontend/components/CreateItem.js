a0091383a92a3ffd2ae02ccb8469298cb42e7abb
William Blake <willblakebooking@gmail.com>
2018-11-29 13:51:22 -0600

    create items with mutation

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
-import Mutation from 'react-apollo';
+import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+      $title: String!
+      $description: String!
+      $price: Int!
+      $image: String
+      $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
 
 class CreateItem extends Component {
   state = {
@@ -21,46 +43,65 @@ class CreateItem extends Component {
 
   render() {
     return (
-      <Form>
-        <fieldset>
-          <label htmlFor='title'>
-            Title
-            <input
-              type='text'
-              id='title'
-              name='title'
-              placeholder='Title'
-              requiredValue={this.state.title}
-              onChange={this.handleChange}
-              />
-          </label>
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
 
-          <label htmlFor='price'>
-            Price
-            <input
-              type='number'
-              id='price'
-              name='price'
-              placeholder='Price'
-              requiredValue={this.state.price}
-              onChange={this.handleChange}
-              />
-          </label>
+        <Form onSubmit={async e => {
+          // Stop the form from submitting
+          e.preventDefault();
+          // Call the mutation
+          const res = await createItem();
+          // Change them to the single item page
+          console.log(res);
+          Router.push({
+            pathname: '/item',
+            query: { id: res.data.createItem.id },
+          })
+        }}>
+        <Error error={error} />
+        <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor='title'>
+              Title
+              <input
+                type='text'
+                id='title'
+                name='title'
+                placeholder='Title'
+                requiredvalue={this.state.title}
+                onChange={this.handleChange}
+                />
+            </label>
 
-          <label htmlFor='description'>
-            Description
-            <textarea
-              id='description'
-              name='description'
-              placeholder='Enter A Description'
-              requiredValue={this.state.description}
-              onChange={this.handleChange}
-              />
-          </label>
-        </fieldset>
-      </Form>
+            <label htmlFor='price'>
+              Price
+              <input
+                type='number'
+                id='price'
+                name='price'
+                placeholder='Price'
+                requiredvalue={this.state.price}
+                onChange={this.handleChange}
+                />
+            </label>
+
+            <label htmlFor='description'>
+              Description
+              <textarea
+                id='description'
+                name='description'
+                placeholder='Enter A Description'
+                requiredvalue={this.state.description}
+                onChange={this.handleChange}
+                />
+            </label>
+            <button type='submit'>Submit</button>
+          </fieldset>
+        </Form>
+        )}
+      </Mutation>
     )
   }
 }
 
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -29,7 +29,7 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
-export default class Items extends Component {
+class Items extends Component {
   render() {
     return (
       <Center>
@@ -46,3 +46,6 @@ export default class Items extends Component {
     )
   }
 }
+
+export default Items;
+export { ALL_ITEMS_QUERY };
