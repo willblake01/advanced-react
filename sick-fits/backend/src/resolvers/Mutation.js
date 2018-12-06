334a1fd8633b690f2d3b375ccc8bababc45b7329
William Blake <willblakebooking@gmail.com>
2018-12-06 16:05:36 -0600

    user signup and permission flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
@@ -37,6 +40,29 @@ const Mutations = {
     // 2. Check if they own that item, or have permissions
     // 3. Delete it!
       return ctx.db.delete.item({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    // Hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // Create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      }, info);
+      // Create the JWT for them
+      const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+      // We set the jwt as a cookie on the response
+      ctx.response.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1yr cookie
+      });
+      // Finally we return the user to the browser
+      return user;
   },
 };
 
