4ea24f1d548587a2c102a177c1bf4f7f7ce7164f
William Blake <willblakebooking@gmail.com>
2018-12-15 16:25:22 -0600

    optomistic response && cache updates with Apollo

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -24,19 +24,45 @@ const BigButton = styled.button`
 `;
 
 class RemoveFromCart extends React.Component {
-  static PropTypes = {
+  static propTypes = {
     id: PropTypes.string.isRequired,
   };
+  // This gets called as soon as we get a response back from the server after a mutation has been performed
+  update = (cache, payload) => {
+    console.log('Running remove from cart update fn');
+    // 1. First read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY});
+    console.log(data);
+    // 2. Remove that item from the cart
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // 3. Write it back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   render() {
-    return <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id: this.props.id }}>{(removeFromCart, { loading, error }) => (
-      <BigButton
-        disabled={loading}
-        onClick={() => {
-          removeFromCart().catch((err) => alert(err.message));
+    return (
+      <Mutation
+        mutation={REMOVE_FROM_CART_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id,
+          }
         }}
-        title='Delete Item'>&times;
-      </BigButton>
-    )}</Mutation>
+      >
+        {(removeFromCart, { loading, error }) => (
+            <BigButton
+              disabled={loading}
+              onClick={() => {
+                removeFromCart().catch((err) => alert(err.message));
+              }}
+              title='Delete Item'>&times;
+            </BigButton>
+        )}</Mutation>
+    )
   }
 }
 
