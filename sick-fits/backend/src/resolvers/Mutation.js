0c0875a33f4e9c29c98d9d692fefae2faf2c944a
William Blake <willblakebooking@gmail.com>
2018-12-18 10:17:10 -0600

    charging cards on the server side

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -4,6 +4,7 @@ const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, makeANiceEmail } = require('../mail');
 const { hasPermission } = require('../utils');
+const stripe = require('../stripe');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -245,6 +246,40 @@ const Mutations = {
       where: { id: args.id },
     }, info);
   },
+  async createOrder(parent, args, ctx, info) {
+    // 1. Query the current user and make sure they are signed in.
+    const { userId } = ctx.request;
+    if(!userId) throw new Error('You must be signed in to complete this order.');
+    const user = await ctx.db.query.user(
+      { where: { id: userId } },
+      `
+      {
+        id
+        name
+        email
+        cart {
+          id
+          quantity
+          item {
+        title price id description image }
+      }}`
+    );
+    // 2. Recalculate the total for the price.
+    const amount = user.cart.reduce(
+      (tally, cartItem) => tally + cartItem.item.price * cartItem.quantity, 0
+    );
+    console.log(`Going to charge the total of ${amount}`);
+    // 3. Create the Stripe charge (turn the token into $$$).
+    const charge = await stripe.charges.create({
+      amount,
+      currency: 'USD',
+      source: args.token,
+    });
+    // 4. Convert the CartItems to OrderItems.
+    // 5. Create the order.
+    // 6. Clean up - clear the user's cart, delete CartItems.
+    // 7. Return the order to the client.
+  }
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -9,28 +9,56 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = (res) => {
+  onToken = (res, createOrder) => {
     console.log('On Token Called!!');
     console.log(res.id);
-  }
+    // Manually call the mutation once we have the Stripe token.
+    createOrder({
+      variables: {
+        token: res.id
+      }
+    }).catch(err => {
+      alert(err.message);
+    });
+  };
   render() {
     return <User>
         {({ data: { me }}) => (
-          <StripeCheckout
-            amount={calcTotalPrice(me.cart)}
-            name='Sick Fits'
-            description={`Order of ${totalItems(me.cart)} items.`}
-            image={me.cart[0].item && me.cart[0].item.image}
-            stripeKey='pk_test_TWkMSIUvATWhLhvIxi8UMBwB'
-            currency='USD'
-            email={me.email}
-            token={res => this.onToken(res)}
-          >{this.props.children}</StripeCheckout>
+          <Mutation mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{CURRENT_USER_QUERY}]}>{(createOrder) => (
+            <StripeCheckout
+              amount={calcTotalPrice(me.cart)}
+              name='Sick Fits'
+              description={`Order of ${totalItems(me.cart)} items.`}
+              image={me.cart[0].item && me.cart[0].item.image}
+              stripeKey='pk_test_TWkMSIUvATWhLhvIxi8UMBwB'
+              currency='USD'
+              email={me.email}
+              token={res => this.onToken(res, createOrder)}
+            >
+              {this.props.children}
+            </StripeCheckout>
+          )}
+          </Mutation>
         )}
       </User>
   }
