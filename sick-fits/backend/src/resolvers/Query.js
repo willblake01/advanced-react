2a0166f932602d0df99d0d21818e33598394a2b7
William Blake <willblakebooking@gmail.com>
2018-12-18 13:16:02 -0600

    displaying single orders

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -26,6 +26,26 @@ const Query = {
     // 3. If they do, query all users
     return ctx.db.query.users({}, info);
   },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in.
+    if (!ctx.request.userId) {
+      throw new Error('You aren\'t logged in');
+    }
+    // 2. Query the current order.
+    const order = await ctx.db.query.order({
+      where: {
+        id: args.id,
+      }
+    }, info);
+    // 3. Check if they have the permissions to see this order.
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder || hasPermission) {
+      throw new Error('You can\'t see this buddy');
+    }
+    // 4. Return the order.
+    return order;
+  },
 };
 
 module.exports = Query;
